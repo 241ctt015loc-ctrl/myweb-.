38b180eff902f7c1c745ccf08f6ee1332dfcba5c
cao huy loc <241ctt015_loc@pyu.edu.vn>
2026-03-18 09:20:31 +0700

    Hoan thanh trang profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,12 @@
     <style>
         body { font-family: sans-serif; background-color: #f0f2f5; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; }
         .card { background: white; padding: 2rem; border-radius: 15px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); text-align: center; }
-        h1 { color: #1877f2; }
         p { color: #555; }
     </style>
 </head>
 <body>
     <div class="card">
-        <h1>CAO HUY LỘC - 241CTT015</h1>
+    
         <p>Đây là bài code đầu tiên trên Laravel .</p>
         <p>Trạng thái: <b>Đã cài đặt thành công!</b></p>
     </div>
